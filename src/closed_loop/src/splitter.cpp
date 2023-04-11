0b57da75f0de4e674fc874cfd74ff54e476364fc
Gregory Holder <gregory.holder76@gmail.com>
2023-04-11 16:19:07 +0200

    splitter

--- src/closed_loop/src/splitter.cpp
+++ src/closed_loop/src/splitter.cpp
@@ -4,23 +4,23 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "asdfr_interfaces/msg/point2
-
-
+#include "asdfr_interfaces/msg/point2.hpp"
 #include "std_msgs/msg/float32.hpp"
 
+using std::placeholders::_1;
+using namespace std::chrono_literals;
 
 class Splitter : public rclcpp::Node
 {
 private:
     rclcpp::Subscription<asdfr_interfaces::msg::Point2>::SharedPtr input_sub_;
-    rclcpp::Publisher<std_msgs::msg::float32>::SharedPtr output_x_pub_;
-    rclcpp::Publisher<std_msgs::msg::float32>::SharedPtr output_y_pub_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr output_x_pub_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr output_y_pub_;
 
     void on_input(const asdfr_interfaces::msg::Point2 msg) {
 
-        std_msgs::msg::float32 x;
-        std_msgs::msg::float32 y;
+        std_msgs::msg::Float32 x;
+        std_msgs::msg::Float32 y;
 
         x.data = msg.x;
         y.data = msg.y;
@@ -37,8 +37,8 @@ public:
         input_sub_ = this->create_subscription<asdfr_interfaces::msg::Point2>(
         "input", 1 , std::bind(&Splitter::on_input, this, _1));
 
-        output_x_pub_ = this->create_publisher<std_msgs::msg::float32>("output_x", 1);
-        output_y_pub_ = this->create_publisher<std_msgs::msg::float32>("output_y", 1);
+        output_x_pub_ = this->create_publisher<std_msgs::msg::Float32>("output_x", 1);
+        output_y_pub_ = this->create_publisher<std_msgs::msg::Float32>("output_y", 1);
     }
 
 };
@@ -47,6 +47,6 @@ int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<Splitter>());
-    rlcpp::shutdown();
+    rclcpp::shutdown();
     return 0;
 }
